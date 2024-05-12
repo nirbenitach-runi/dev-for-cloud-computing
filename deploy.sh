18481bcc9a9cea1ebc625bd2c9f7e9336baad27f
Nir Ben Itach <nir@insoundz.com>
2024-05-12 15:25:31 +0300

    Remove any commands from the deploy script that are not relevant for the initial deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 terraform init
-# AWS_PROFILE="runi" terraform import aws_dynamodb_table.parking_lot_table ParkingLotTable
-# AWS_PROFILE="runi" terraform import aws_iam_role.lambda_role lambda_execution_role
 AWS_PROFILE="runi" terraform apply -auto-approve
