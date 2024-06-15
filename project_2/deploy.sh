58606a1eb0fb73e6eb6ad33b054714ee2c5ac60d
Nir Ben Itach <nir@insoundz.com>
2024-06-15 13:24:55 +0300

    Align the deploy.sh file for general use

--- project_2/deploy.sh
+++ project_2/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 terraform init
-AWS_PROFILE="runi" terraform import aws_dynamodb_table.users_table Users
-AWS_PROFILE="runi" terraform import aws_dynamodb_table.groups_table Groups
-AWS_PROFILE="runi" terraform import aws_dynamodb_table.messages_table Messages
-AWS_PROFILE="runi" terraform apply -auto-approve
+AWS_PROFILE="your-aws-profile" terraform apply -auto-approve
